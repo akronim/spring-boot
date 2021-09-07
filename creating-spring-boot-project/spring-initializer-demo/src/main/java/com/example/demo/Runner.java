4ca3aca7c2d25387dfd7f3843a3bf9b2cbe69583
acronim <marinko.miskovic.hr@gmail.com>
2021-09-07 19:54:59 +0200

    application.properties

--- creating-spring-boot-project/spring-initializer-demo/src/main/java/com/example/demo/Runner.java
+++ creating-spring-boot-project/spring-initializer-demo/src/main/java/com/example/demo/Runner.java
@@ -1,14 +1,27 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Runner implements CommandLineRunner {
 
+    // value of "foo" comes from application.properties file
+    @Value("${foo}")
+    private String foo;
+
+    @Value("${numbers}")
+    private int[] numbers;
+
     @Override
     public void run(String... args) throws Exception {
         System.out.println("Hello From Bean CommandLine");
+        System.out.println(foo);
+
+        for (int number : numbers) {
+            System.out.println(number);
+        }
     }
 
 }
