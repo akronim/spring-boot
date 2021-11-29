10642c435c6502c6dac60c456f61ae7797ecbc10
acronim <marinko.miskovic.hr@gmail.com>
2021-11-29 20:05:11 +0100

    test experiments

---
+++ testing-3/src/main/java/com/example/repository/HelloRepositoryImpl.java
@@ -0,0 +1,11 @@
+package com.example.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class HelloRepositoryImpl implements HelloRepository {
+    @Override
+    public String get() {
+        return "Hello JUnit 5";
+    }
+}
---
+++ testing-3/src/main/java/com/example/services/HelloService.java
@@ -0,0 +1,7 @@
+package com.example.services;
+
+public interface HelloService {
+
+    String get();
+
+}
