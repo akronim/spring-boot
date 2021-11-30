3dfa871103a591e0d93ff1075d635df271bc1a9e
acronim <marinko.miskovic.hr@gmail.com>
2021-11-30 22:09:35 +0100

    more testing

--- testing-3/src/main/java/com/example/controller/MainController.java
+++ testing-3/src/main/java/com/example/controller/MainController.java
@@ -2,7 +2,15 @@ package com.example.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.validation.BindingResult;
+
+import javax.validation.Valid;
+
+import com.example.entities.Input;
 
 @Controller
 public class MainController {
@@ -11,6 +19,16 @@ public class MainController {
     @GetMapping("/")
     public String hello() {
         return "Hello Controller";
+    }
+
+    @PostMapping("/validate-1")
+    public String validate1(@RequestParam(value = "channel") String channel,
+            @ModelAttribute @Valid Input input, BindingResult result) {
+
+        if (result.hasErrors()) {
+            return "has errors";
+        }
+        return "no errors";
     }
 
 }
