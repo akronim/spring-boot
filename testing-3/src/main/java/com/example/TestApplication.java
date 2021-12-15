fd28af0c6313bf61ff3ffc44ff78116e0351367a
Marinko Mišković <mmiskovic@hattrick.local>
2021-12-15 09:18:30 +0100

    bugfix

---
+++ testing-3/src/main/java/com/example/TestApplication.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class TestApplication {
+    public static void main(String[] args) {
+		SpringApplication.run(TestApplication.class, args);
+	}
+}
