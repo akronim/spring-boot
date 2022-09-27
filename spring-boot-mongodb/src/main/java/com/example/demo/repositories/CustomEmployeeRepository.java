da686ac5f459c03c399f93e5119183d816c44147
mismar <miskovic.marinko@feg.eu>
2022-09-27 09:13:19 +0200

    delete a project

--- spring-boot-mongodb/src/main/java/com/example/demo/repositories/CustomEmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repositories;
-
-import com.example.demo.entities.Employee;
-
-public interface CustomEmployeeRepository {
-    void testMethod(String employeeId, Employee employee);
-}
